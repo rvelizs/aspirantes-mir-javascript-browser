0cf6ea4068a766bbeb13e1819cac708f0bf99fe9
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2023-05-29 18:32:54 -0500

    avance

---
+++ js-brow-2/js/script.js
@@ -0,0 +1,22 @@
+/*const btn_agregar = document.getElementById("btnagregar");
+
+btn_agregar.addEventListener("click", function () {
+    const titulo_tarea = document.getElementById("tarea").value;
+    document.getElementById("tit").innerHTML = titulo_tarea;
+});*/
+
+const btn = document.getElementById("btn");
+
+function handleSubmit (event) {
+    btn.addEventListener("click", function () {
+        
+    });
+}
+
+function handleCheckTask (event) {
+
+}
+
+function handleDelete (event) {
+
+}
