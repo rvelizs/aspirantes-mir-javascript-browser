a63c44be0f4864527306a197f424684ed84ca711
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2023-05-31 18:18:50 -0500

    error appendchild

--- js-brow-2/js/script.js
+++ js-brow-2/js/script.js
@@ -1,16 +1,33 @@
-/*const btn_agregar = document.getElementById("btnagregar");
+let numTasks = 1;
 
-btn_agregar.addEventListener("click", function () {
-    const titulo_tarea = document.getElementById("tarea").value;
-    document.getElementById("tit").innerHTML = titulo_tarea;
-});*/
+const obj = {
+    id: numTasks,
+    title: "",
+    completed: false
+}
 
 const btn = document.getElementById("btn");
+btn.addEventListener("click", handleSubmit);
+
+const task = document.getElementById("task");
+
 
 function handleSubmit (event) {
-    btn.addEventListener("click", function () {
-        
-    });
+    event.preventDefault();
+    // objeto Task
+    obj.id = numTasks++;
+    obj.title = task.value;
+    obj.completed = false;
+    console.log(obj);
+    createTask(obj);
+    //input.addEventListener("click", handleCheckTask);
+}
+
+function createTask (x) {
+    const ul = document.getElementById("list");
+    const li = document.createElement("li");
+    li.appendChild(ul);
+    li.textContent(x.title);
 }
 
 function handleCheckTask (event) {
